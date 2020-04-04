47265100a6f33f14695403fe30063f7a893d85c4
kbariotis <konmpar@gmail.com>
2020-04-04 19:40:48 +0100

    remove commented code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -852,10 +852,7 @@ export class App extends React.Component<any, AppState> {
       this.roomID = roomMatch[1];
       this.roomKey = roomMatch[2];
       this.socket.on("init-room", () => {
-        // this.socket && this.socket.emit("join-room", this.roomID);
-        if (this.socket) {
-          this.socket.emit("join-room", this.roomID);
-        }
+        this.socket && this.socket.emit("join-room", this.roomID);
       });
       this.socket.on(
         "client-broadcast",
